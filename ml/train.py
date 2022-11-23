15c890fc74cd2ed9cded19ed5549390b5ed07b94
Pierre DM <pdaixmoreux@gmail.com>
2022-11-23 17:58:47 +0100

    remove save_name arg

--- ml/train.py
+++ ml/train.py
@@ -42,4 +42,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--nlines", type=int)
     args = parser.parse_args()
-    exit(main(args.nlines, args.save_name))
+    exit(main(args.nlines))
